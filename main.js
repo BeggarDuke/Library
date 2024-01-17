7cf82787084eacf948a6b02b148a87dc7f42e869
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2024-01-18 01:05:40 +0300

    add: Improve function that adds a book cards

--- main.js
+++ main.js
@@ -11,38 +11,52 @@ function Book(title, author, pages, read) {
   this.info = function() {
     return `${this.title}, ${this.author}, ${this.pages}, ${this.read}.`;
   }
+  this.number = undefined;
 }
 
 function addToLibrary() {
   let newBook = prompt("Title, author, pages, read").split(", ");
   myLibrary.push(new Book(...newBook));
-  addVisual();
+  addVisual(true);
 }
 
-function addVisual() {
-  let divs = document.createElement("div");
-  divs.className = `Book${myLibrary.length-1}`
-  let rmbtn = document.createElement("button");
-  rmbtn.innerText = "Remove";
-  for (i=0; i < 8; i++) {
-    divs.appendChild(document.createElement("div"));
+function addVisual(add) {
+  // This "if" statement is needed for this function to know whether to loop over the whole array or just the last added item.
+  // Either "text" and "set" variables are needed to choose between books id naming method for whole array or just last added book.
+  let set;
+  if (!add && myLibrary.length === 0) {return}
+  else if (!add && myLibrary.length > 0) {set = myLibrary.length}
+  else {set = 1}
+  for (i=0; i < set; i++) {
+    console.log(set);
+    let text = !add ? i : myLibrary.length-1;
+    console.log(text);
+    let divs = document.createElement("div");
+    myLibrary[text].number = `Book${text}`;
+    divs.className = `Book${text}`;
+    let rmbtn = document.createElement("button");
+    rmbtn.innerText = "Remove";
+    for (i=0; i < 8; i++) {
+      divs.appendChild(document.createElement("div"));
+    }
+    divs.querySelector("div:nth-child(1)").innerText = `Title:`;
+    divs.querySelector("div:nth-child(2)").innerText = `Author:`;
+    divs.querySelector("div:nth-child(3)").innerText = `Pages:`;
+    divs.querySelector("div:nth-child(4)").innerText = `Read:`;
+    divs.querySelector("div:nth-child(5)").innerText = `${myLibrary[text].title}`;
+    divs.querySelector("div:nth-child(6)").innerText = `${myLibrary[text].author}`;
+    divs.querySelector("div:nth-child(7)").innerText = `${myLibrary[text].pages}`;
+    divs.querySelector("div:nth-child(8)").innerText = `${myLibrary[text].read}`;
+    divs.appendChild(rmbtn);
+    lib.appendChild(divs);
+    removeBook(divs, rmbtn);
   }
-  divs.querySelector("div:nth-child(1)").innerText = `Title:`;
-  divs.querySelector("div:nth-child(2)").innerText = `Author:`;
-  divs.querySelector("div:nth-child(3)").innerText = `Pages:`;
-  divs.querySelector("div:nth-child(4)").innerText = `Read:`;
-  divs.querySelector("div:nth-child(5)").innerText = `${myLibrary[myLibrary.length-1].title}`;
-  divs.querySelector("div:nth-child(6)").innerText = `${myLibrary[myLibrary.length-1].author}`;
-  divs.querySelector("div:nth-child(7)").innerText = `${myLibrary[myLibrary.length-1].pages}`;
-  divs.querySelector("div:nth-child(8)").innerText = `${myLibrary[myLibrary.length-1].read}`;
-  divs.appendChild(rmbtn);
-  lib.appendChild(divs);
-  removeBook(divs, rmbtn);
 }
 
 function removeBook(divs, rmbtn){
   rmbtn.addEventListener("click", () => {
-    myLibrary.splice(+divs.className.replace("Book", ""), 1);
+    console.log(divs.className);
+    myLibrary.splice(myLibrary.findIndex((item) => item.number === divs.className), 1);
     divs.remove();
   });
 }
