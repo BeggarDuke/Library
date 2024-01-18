4b89d1b8aa30fdc865838da660cce90932476ce6
BeggarDuke <107742417+BeggarDuke@users.noreply.github.com>
2024-01-18 19:04:22 +0300

    fix: improve cards for better styling

--- main.js
+++ main.js
@@ -22,32 +22,32 @@ function addToLibrary() {
 
 function addVisual(add) {
   // This "if" statement is needed for this function to know whether to loop over the whole array or just the last added item.
-  // Either "text" and "set" variables are needed to choose between books id naming method for whole array or just last added book.
+  // Either "text" and "set" variables are needed to choose between books id naming method for whole array or just the last added book.
   let set;
   if (!add && myLibrary.length === 0) {return}
   else if (!add && myLibrary.length > 0) {set = myLibrary.length}
   else {set = 1}
   for (i=0; i < set; i++) {
-    console.log(set);
     let text = !add ? i : myLibrary.length-1;
-    console.log(text);
     let divs = document.createElement("div");
     myLibrary[text].number = `Book${text}`;
     divs.className = `Book${text}`;
     let rmbtn = document.createElement("button");
     rmbtn.innerText = "Remove";
-    for (i=0; i < 8; i++) {
+    for (i=1; i <= 8; i++) {
       divs.appendChild(document.createElement("div"));
+      divs.querySelector(`div:nth-child(${i})`).appendChild(document.createElement("p"));
     }
-    divs.querySelector("div:nth-child(1)").innerText = `Title:`;
-    divs.querySelector("div:nth-child(2)").innerText = `Author:`;
-    divs.querySelector("div:nth-child(3)").innerText = `Pages:`;
-    divs.querySelector("div:nth-child(4)").innerText = `Read:`;
-    divs.querySelector("div:nth-child(5)").innerText = `${myLibrary[text].title}`;
-    divs.querySelector("div:nth-child(6)").innerText = `${myLibrary[text].author}`;
-    divs.querySelector("div:nth-child(7)").innerText = `${myLibrary[text].pages}`;
-    divs.querySelector("div:nth-child(8)").innerText = `${myLibrary[text].read}`;
-    divs.appendChild(rmbtn);
+    divs.querySelector("div:nth-child(1) > p").innerText = `Title:`;
+    divs.querySelector("div:nth-child(2) > p").innerText = `Author:`;
+    divs.querySelector("div:nth-child(3) > p").innerText = `Pages:`;
+    divs.querySelector("div:nth-child(4) > p").innerText = `Read:`;
+    divs.querySelector("div:nth-child(5) > p").innerText = `${myLibrary[text].title}`;
+    divs.querySelector("div:nth-child(6) > p").innerText = `${myLibrary[text].author}`;
+    divs.querySelector("div:nth-child(7) > p").innerText = `${myLibrary[text].pages}`;
+    divs.querySelector("div:nth-child(8) > p").innerText = `${myLibrary[text].read}`;
+    divs.appendChild(document.createElement("div"));
+    divs.querySelector("div:nth-child(9)").appendChild(rmbtn);
     lib.appendChild(divs);
     removeBook(divs, rmbtn);
   }
@@ -55,7 +55,6 @@ function addVisual(add) {
 
 function removeBook(divs, rmbtn){
   rmbtn.addEventListener("click", () => {
-    console.log(divs.className);
     myLibrary.splice(myLibrary.findIndex((item) => item.number === divs.className), 1);
     divs.remove();
   });
